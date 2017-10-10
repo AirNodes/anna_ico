79e87a2f8b5935c5819026efb01c2a41dfa15b20
d6bels <kevin@airnodes.com>
2017-10-10 16:20:49 +0200

    Release block time and initial token supply

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,10 @@ var AnnaCrowdsale = artifacts.require("./AnnaCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
   const startBlock = web3.eth.blockNumber + 2 // blockchain block number where the crowdsale will start
-  const endBlock = startBlock + 151300 // (53 * 24 * 3600 / 30.27)  // blockchain block number where it will end. A block is more or less 30s, https://etherscan.io/chart/blocktime
+  const endBlock = startBlock + 236330 // (93 * 24 * 3600 / 34)  // blockchain block number where it will end. A block is more or less 30s, https://etherscan.io/chart/blocktime
   const rate = new web3.BigNumber(5000) // rate of ether to Anna Coin
   const wallet = web3.eth.accounts[0] // the address that will hold the fund
-  const initial_supply = 5000000e18 // Initial supply for token owner in wei
+  const initial_supply = 10000000e18 // Initial supply for token owner in wei, extra will be burnt
 
   deployer.deploy(AnnaCrowdsale, startBlock, endBlock, rate, wallet, initial_supply)
 }
