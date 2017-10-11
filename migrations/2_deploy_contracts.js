f9a567cabe25981305d17c4211b8d9dfb847cc4d
d6bels <kevin@airnodes.com>
2017-10-11 08:49:24 +0200

    Adjust end time

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var AnnaCrowdsale = artifacts.require("./AnnaCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
   const startBlock = web3.eth.blockNumber + 2 // blockchain block number where the crowdsale will start
-  const endBlock = startBlock + 236330 // (93 * 24 * 3600 / 34)  // blockchain block number where it will end. A block is more or less 30s, https://etherscan.io/chart/blocktime
+  const endBlock = startBlock + 233800 // (~ 92 * 24 * 3600 / 34)  // blockchain block number where it will end. A block is more or less 30s, https://etherscan.io/chart/blocktime
   const rate = new web3.BigNumber(5000) // rate of ether to Anna Coin
   const wallet = web3.eth.accounts[0] // the address that will hold the fund
   const initial_supply = 10000000e18 // Initial supply for token owner in wei, extra will be burnt
